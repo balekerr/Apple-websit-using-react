02d9d61103064c7bc4b441ac5de226f6f8a2ba60
balekerr <97841180+balekerr@users.noreply.github.com>
2022-02-07 23:39:44 -0500

    built a website using react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './Resource/css/styles.css'
+import Header from './component/header/Header'
+import Footer from './component/footer/Footer'
+import Alert from './component/main/Alert';
+import FirstSection from './component/main/FirstSection';
+import SecondSection from './component/main/SecondSection';
+import ThirdSection from './component/main/ThirdSection';
+import FourthSection from './component/main/FourthSection';
+import FifthSection from './component/main/FifthSection';
+import SixSection from './component/main/SixSection';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return( 
+  <div>
+<Header/>
+<Alert/> 
+<FirstSection/>
+<SecondSection/> 
+<ThirdSection/>
+<FourthSection/>
+<FifthSection/>
+<SixSection/>
+ <Footer/> 
+  
+
+  </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // import logo from './logo.svg';
+// // import './App.css';
+
+// // function App() {
+// //   return (
+// //     <div className="App">
+// //       <header className="App-header">
+// //         {/* <img src={logo} className="App-logo" alt="logo" />
+// //         <p>
+// //           Edit <code>src/App.js</code> and save to reload.
+// //         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a> */}
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
